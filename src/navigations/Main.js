a4637b13718c3742d57485c46989e7e2b22b4b43
ayushajay-kiwi <ayush.ajay@kiwitech.com>
2022-09-29 11:26:52 +0530

    conflict solved

--- src/navigations/Main.js
+++ src/navigations/Main.js
@@ -38,8 +38,7 @@ const Main = () => {
 
 
       <Stack.Navigator 
-      // initialRouteName={getRoute(auth.access_token,auth.role_id,auth.registration_step)}
-      initialRouteName='SmDonorSettings'
+      initialRouteName={getRoute(auth.access_token,auth.role_id,auth.registration_step)}
       >
 
       <Stack.Screen
