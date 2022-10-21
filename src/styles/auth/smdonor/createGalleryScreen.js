3ef77736999d7f03b06c2b71907683ac356932d1
laveena <laveena.chaturvedi@kiwitech.com>
2022-10-21 23:51:00 +0530

    component for video uploading

--- src/styles/auth/smdonor/createGalleryScreen.js
+++ src/styles/auth/smdonor/createGalleryScreen.js
@@ -37,9 +37,6 @@ export default {
     tintColor: Colors.BLACK,
     maxWidth: Value.CONSTANT_VALUE_25,
     resizeMode: Alignment.CONTAIN,
-    // alignItems: Alignment.CENTER,
-    // justifyContent: Alignment.CENTER,
-    // alignSlef:'center'
   },
   videoContainer: {
     width: width,
