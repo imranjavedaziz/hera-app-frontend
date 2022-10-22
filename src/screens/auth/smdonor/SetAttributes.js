ca810e75d5cf9980821347cffe2f942f80d2e81d
laveena <laveena.chaturvedi@kiwitech.com>
2022-10-22 12:26:35 +0530

    sonar bugs

--- src/screens/auth/smdonor/SetAttributes.js
+++ src/screens/auth/smdonor/SetAttributes.js
@@ -12,14 +12,10 @@ import Strings from '../../../constants/Strings';
 import {smSetAttributesSchema} from '../../../constants/schemas';
 import BottomSheetComp from '../../../components/BottomSheet';
 import Dropdown from '../../../components/inputs/Dropdown';
-import Auth from '../../../services/Auth';
 import {Value} from '../../../constants/FixedValues';
 import {useDispatch, useSelector} from 'react-redux';
-import SetterData from '../../../services/SetterData';
-import {getStates} from '../../../redux/actions/Register';
 import {hideAppLoader, showAppLoader} from '../../../redux/actions/loader';
 import {getAttribute, saveAttribute} from '../../../redux/actions/SetAttribute';
-import SetAttribute from '../../../redux/reducers/SetAttribute';
 import {logOut} from '../../../redux/actions/Auth';
 import {Routes} from '../../../constants/Constants';
 import {useNavigation} from '@react-navigation/native';
@@ -30,7 +26,6 @@ const SetAttributes = ({route}) => {
   const navigation = useNavigation();
   const [isOpen, setOpen] = useState(false);
   const [attributeData, setAttributeData] = useState();
-  const authService = Auth();
   const dispatch = useDispatch();
   const {
     handleSubmit,
@@ -40,7 +35,6 @@ const SetAttributes = ({route}) => {
     resolver: yupResolver(smSetAttributesSchema),
   });
   const onSubmit = data => {
-    console.log('SUBMIT', data);
     dispatch(saveAttribute(data));
   };
   React.useEffect(() => {
@@ -122,7 +116,7 @@ const SetAttributes = ({route}) => {
             render={({field: {onChange, value}}) => (
               <Dropdown
                 label={Strings.sm_set_attributes.Height}
-                data={set_attribute_res?.height}
+                data={attributeData?.height}
                 onSelect={selectedItem => {
                   onChange(selectedItem.id);
                 }}
@@ -145,7 +139,7 @@ const SetAttributes = ({route}) => {
             render={({field: {onChange}}) => (
               <Dropdown
                 label={Strings.sm_set_attributes.Race}
-                data={set_attribute_res?.race}
+                data={attributeData?.race}
                 onSelect={selectedItem => {
                   onChange(selectedItem.id);
                 }}
@@ -160,7 +154,7 @@ const SetAttributes = ({route}) => {
             render={({field: {onChange}}) => (
               <Dropdown
                 label={Strings.sm_set_attributes.MotherEthnicity}
-                data={set_attribute_res?.ethnicity}
+                data={attributeData?.ethnicity}
                 onSelect={selectedItem => {
                   onChange(selectedItem.id);
                 }}
@@ -175,7 +169,7 @@ const SetAttributes = ({route}) => {
             render={({field: {onChange}}) => (
               <Dropdown
                 label={Strings.sm_set_attributes.FatheEthnicity}
-                data={set_attribute_res?.ethnicity}
+                data={attributeData?.ethnicity}
                 onSelect={selectedItem => {
                   onChange(selectedItem.id);
                 }}
@@ -190,7 +184,7 @@ const SetAttributes = ({route}) => {
             render={({field: {onChange}}) => (
               <Dropdown
                 label={Strings.sm_set_attributes.Weight}
-                data={set_attribute_res?.weight}
+                data={attributeData?.weight}
                 onSelect={selectedItem => {
                   onChange(selectedItem.id);
                 }}
@@ -211,7 +205,7 @@ const SetAttributes = ({route}) => {
             render={({field: {onChange}}) => (
               <Dropdown
                 label={Strings.sm_set_attributes.EyeColor}
-                data={set_attribute_res?.eye_colour}
+                data={attributeData?.eye_colour}
                 onSelect={selectedItem => {
                   onChange(selectedItem.id);
                 }}
@@ -226,7 +220,7 @@ const SetAttributes = ({route}) => {
             render={({field: {onChange}}) => (
               <Dropdown
                 label={Strings.sm_set_attributes.HairColor}
-                data={set_attribute_res?.hair_colour}
+                data={attributeData?.hair_colour}
                 onSelect={selectedItem => {
                   onChange(selectedItem.id);
                 }}
@@ -241,7 +235,7 @@ const SetAttributes = ({route}) => {
             render={({field: {onChange}}) => (
               <Dropdown
                 label={Strings.sm_set_attributes.Education}
-                data={set_attribute_res?.education}
+                data={attributeData?.education}
                 onSelect={selectedItem => {
                   onChange(selectedItem.id);
                 }}
