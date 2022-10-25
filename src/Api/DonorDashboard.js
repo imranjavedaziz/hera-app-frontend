8504a9255aab3363e0efe63321e09ccf48d6b2fa
laveena <laveena.chaturvedi@kiwitech.com>
2022-10-25 12:23:43 +0530

    search

--- src/Api/DonorDashboard.js
+++ src/Api/DonorDashboard.js
@@ -2,7 +2,6 @@ import axiosRequest from '../utils/axiosRequest';
 import ApiPath from '../constants/ApiPath';
 
 export const DonorDashboardApi = (payload) => {
-  console.log(payload, "payload:::::");
   return axiosRequest.get(
     `${ApiPath.ptbProfileCard}?keyword=${payload?.keyword}&state_ids${payload?.state_ids}&limit=${payload?.limit}&page=${payload.page}`
   );
