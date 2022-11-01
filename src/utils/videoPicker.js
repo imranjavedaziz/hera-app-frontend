51a63002319dddb8fe930e5152320a939217bcb3
laveena <laveena.chaturvedi@kiwitech.com>
2022-11-01 13:47:26 +0530

    image

--- src/utils/videoPicker.js
+++ src/utils/videoPicker.js
@@ -13,10 +13,8 @@ const videoPicker = index => {
   return new Promise((resolve, reject) => {
     myAction(options)
       .then(video => {
-        console.log(video?.duration, "video?.duration");
         let duration = ((video?.duration % 60000) / 1000).toFixed(0);
-        console.log(duration, "duration");
-        if (duration > 60) {
+        if (duration > 59) {
           alert(Strings.sm_create_gallery.videoDuration);
         } else {
           resolve(video);
