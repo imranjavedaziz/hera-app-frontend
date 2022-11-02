76d40e59be9965c77b236dc19a7340522732e993
laveena <laveena.chaturvedi@kiwitech.com>
2022-11-02 10:38:31 +0530

    image

--- src/utils/videoPicker.js
+++ src/utils/videoPicker.js
@@ -2,11 +2,8 @@
 import React from 'react';
 import ImagePicker from 'react-native-image-crop-picker';
 import Strings from '../constants/Strings';
-import { useDispatch } from "react-redux";
-import { showAppToast } from "../redux/actions/loader";
 
 const videoPicker = index => {
-  const dispatch = useDispatch();
   const myAction =
     index === 1 ? ImagePicker.openPicker : ImagePicker.openCamera;
   const options = {
@@ -17,7 +14,7 @@ const videoPicker = index => {
     myAction(options)
       .then(video => {
         if (video?.duration > 59000) {
-          dispatch(showAppToast(true, Strings.sm_create_gallery.videoDuration));
+          alert(Strings.sm_create_gallery.videoDuration)
         } else {
           resolve(video);
         }
