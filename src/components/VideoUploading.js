55919694bb77bb0d9b1463655d995548d558431e
Prerna Sharma <97276501+Prerna-Kiwi@users.noreply.github.com>
2022-11-18 19:16:56 +0530

    'Ui-fixes'

--- src/components/VideoUploading.js
+++ src/components/VideoUploading.js
@@ -12,11 +12,15 @@ import Strings from '../constants/Strings';
 import Video from 'react-native-video';
 import Images from '../constants/Images';
 import FastImage from 'react-native-fast-image';
-import {Alignment} from '../constants';
+import {Alignment, Colors} from '../constants';
+import {MaterialIndicator} from 'react-native-indicators';
 
 const VideoUploading = props => {
   return (
     <TouchableOpacity onPress={() => props?.onPress()}>
+      {props?.video?.loading && (
+        <MaterialIndicator color={Colors.COLOR_A3C6C4} />
+      )}
       <FastImage style={props?.style}>
         {props?.video?.file_url !== '' ? (
           <>
