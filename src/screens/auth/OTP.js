24f518021344d2e1724a542c8960b3fab1785b95
Raji Ahmad <raji.ahmad@kiwitech.com>
2022-12-01 11:18:38 +0530

    Fixed subscription blocker and added resend otp functionality

--- src/screens/auth/OTP.js
+++ src/screens/auth/OTP.js
@@ -22,14 +22,14 @@ import OtpInputs from '../../components/OtpInputs';
 import {otpSchema} from '../../constants/schemas';
 import {height} from '../../utils/responsive';
 import styles from '../../styles/auth/otpScreen';
-import {verifyOtp} from '../../redux/actions/Auth';
+import {verifyOtp,mobileNumber} from '../../redux/actions/Auth';
 import { verifyEmail } from '../../redux/actions/VerificationMail';
 import {useDispatch, useSelector} from 'react-redux';
-
 import {hideAppLoader, showAppLoader,showAppToast} from '../../redux/actions/loader';
 import {Routes} from '../../constants/Constants';
 import {Value} from '../../constants/FixedValues';
 import {Colors} from '../../constants';
+import { sendVerificationMail } from '../../redux/actions/VerificationMail';
 
 const OTP = ({route}) => {
   const dispatch = useDispatch();
@@ -51,11 +51,25 @@ const OTP = ({route}) => {
 
   const {verify_otp_success, verify_otp_loading, verify_otp_error_msg} =
     useSelector(state => state.Auth);
-    const {
-      verify_mail_success,
-      verify_mail_loading,
-      verify_mail_error_msg,
-      verify_mail_res} = useSelector(state=>state.VerificationMail);
+  const {
+    verify_mail_success,
+    verify_mail_loading,
+    verify_mail_error_msg,
+    verify_mail_res} = useSelector(state=>state.VerificationMail);
+
+  const {
+    mobile_number_success,
+    mobile_number_loading,
+    mobile_number_error_msg,
+    register_user_success_data,
+  } = useSelector(state => state.Auth);
+
+  const {
+    send_verification_success,
+    send_verification_loading,
+    send_verification_error_msg,
+    send_verification_res,
+  } = useSelector(state => state.VerificationMail);
   // send otp res
   useEffect(() => {
     if (loadingRef.current && !verify_otp_loading) {
@@ -99,6 +113,37 @@ const OTP = ({route}) => {
     verify_mail_success,
     verify_mail_loading,
     verify_mail_error_msg,
+  ]);
+  useEffect(() => {
+    if (loadingRef.current && !mobile_number_loading) {
+      dispatch(showAppLoader());
+      if (mobile_number_success) {
+        dispatch(hideAppLoader());
+        dispatch(showAppToast(false,'OTP send again successfully!'));
+      }
+      if (mobile_number_error_msg) {
+        dispatch(hideAppLoader());
+      }
+    }
+    loadingRef.current = mobile_number_loading;
+  }, [mobile_number_success, mobile_number_loading, register_user_success_data]);
+  useEffect(() => {
+    if (loadingRef.current && !send_verification_loading) {
+      dispatch(showAppLoader());
+      if (send_verification_success) {
+        dispatch(hideAppLoader());
+        dispatch(showAppToast(false, send_verification_res.message));
+      }
+      if (send_verification_error_msg) {
+        dispatch(hideAppLoader());
+      }
+    }
+    loadingRef.current = send_verification_loading;
+  }, [
+    send_verification_success,
+    send_verification_loading,
+    send_verification_res,
+    send_verification_error_msg,
   ]);
   const onSubmit = data => {
     if( type ===1 || type ===2 ){
@@ -145,6 +190,20 @@ const OTP = ({route}) => {
       accessibilityLabel="Left arrow Button, Press to go back"
     />
   );
+  const resendOTP = ()=>{
+    if(type===1 || type===2){
+      const payload = {
+        country_code: isRouteData.country_code,
+        phone_no: isRouteData.phone_no,
+        type
+      };
+      dispatch(showAppLoader());
+      dispatch(mobileNumber(payload));
+    }
+    else{
+      dispatch(sendVerificationMail());
+    }
+  }
   return (
     <View
       style={{
@@ -215,7 +274,7 @@ const OTP = ({route}) => {
                         : styles.troubleRow
                     }>
                     <Text style={styles.trouble}>{Strings.otp.Trouble}</Text>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={resendOTP}>
                       <Text style={styles.resend}>{Strings.otp.SendAgain}</Text>
                     </TouchableOpacity>
                   </View>
