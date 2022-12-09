0c18b9a534c744c2157cb2e6a0c84f988bf76683
laveena <laveena.chaturvedi@kiwitech.com>
2022-12-09 19:20:12 +0530

    "changes"

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -21,7 +21,7 @@ const environment = {
   },
 };
 
-export const {bucket, api_url, chat} = environment.qa;
+export const {bucket, api_url, chat} = environment.stage;
 
 const WEB_BASE_URL = 'https://makingbabyconnection.com/';
 export const ABOUT_URL = `${WEB_BASE_URL}about`;
