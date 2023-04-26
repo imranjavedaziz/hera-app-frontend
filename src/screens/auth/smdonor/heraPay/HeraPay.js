8d06e4a628c77881b1a8cddd54e2eea0ad7e1162
Prerna-Kiwi <Prerna.sharma@kiwitech.com>
2023-04-26 15:50:09 +0530

    'Hera Pay'

--- src/screens/auth/smdonor/heraPay/HeraPay.js
+++ src/screens/auth/smdonor/heraPay/HeraPay.js
@@ -1,12 +1,160 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from 'react-native';
+import {Container, ModalMiddle} from '../../../../components';
+import {IconHeader} from '../../../../components/Header';
+import {Images, Strings} from '../../../../constants';
+import styles from './style';
+import {useNavigation} from '@react-navigation/native';
+import CustomModal from '../../../../components/CustomModal/CustomModal';
+import AboutPayment from '../../../../components/AboutPayment/AboutPayment';
+import PaymentComp from '../../../../components/PaymentComp/PaymentComp';
+import {ValidationMessages} from '../../../../constants/Strings';
 
 const HeraPay = () => {
-  return (
-    <View>
-      <Text>HeraPay</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
+  const [modal, setModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-export default HeraPay
+  const headerComp = () => (
+    <IconHeader
+      leftIcon={Images.circleIconBack}
+      leftPress={() => navigation.goBack()}
+      style={styles.headerIcon}
+      rightIcon={Images.I_BUTTON}
+      rightPress={() => setModal(!modal)}
+    />
+  );
+
+  const backAction = () => {
+    Alert.alert(
+      Strings.Hera_Pay.Remove_Card,
+      Strings.Hera_Pay.Remove_Card_Text,
+      [
+        {
+          text: Strings.Hera_Pay.Yes_Remove,
+        },
+        {
+          text: Strings.Hera_Pay.Not_Now,
+          onPress: () => null,
+        },
+      ],
+    );
+    return true;
+  };
+  return (
+    <>
+      <Container
+        scroller={false}
+        showHeader={true}
+        headerComp={headerComp}
+        mainStyle={true}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="handled">
+          <View style={styles.container}>
+            <Text style={styles.heraPay}>{Strings.Hera_Pay.HERA_PAY}</Text>
+            <Text style={styles.sendPay}>{Strings.Hera_Pay.Send_Payment}</Text>
+            <TouchableOpacity style={styles.btnContainer}>
+              <Text style={styles.btnText}>
+                {Strings.Hera_Pay.MAKE_PAYMENT}
+              </Text>
+            </TouchableOpacity>
+            <View style={styles.paymentReqContainer}>
+              <PaymentComp
+                Icon={Images.MONEY_TRANSFER}
+                Heading={Strings.Payment_Comp.See_Payment_Request}
+                Content={Strings.Payment_Comp.Request_Description}
+                Pending={Strings.Payment_Comp.Pending_Request}
+                line
+              />
+            </View>
+            <View style={styles.historyContainer}>
+              <PaymentComp
+                Icon={Images.TRANSACTION_HISTORY}
+                Heading={Strings.Payment_Comp.Transaction_History}
+                Content={Strings.Payment_Comp.History_Description}
+                line
+              />
+            </View>
+            <View style={styles.cerditCardContainer}>
+              <PaymentComp
+                payment
+                Icon={Images.CREDIT_CARD}
+                Heading={Strings.Payment_Comp.Manage_Card}
+              />
+              <View style={styles.cardsContainer}>
+                <Image source={Images.ICON_MASTER} />
+                <View style={styles.cardsInner}>
+                  <Text style={styles.cardNo}>
+                    {Strings.Hera_Pay.CARD_NUMBER}
+                  </Text>
+                  <Text style={styles.cardTime}>
+                    {Strings.Hera_Pay.CARD_TIME}
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  onPress={() => {
+                    Platform.OS === 'ios' ? backAction() : setShowModal(true);
+                  }}>
+                  <Image source={Images.iconDarkMore} />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.cardsTwoContainer}>
+                <Image source={Images.VISA_LOGO} />
+                <View style={styles.cardsInner}>
+                  <Text style={styles.cardNo}>
+                    {Strings.Hera_Pay.CARD_NUM_TWO}
+                  </Text>
+                  <Text style={styles.cardTime}>
+                    {Strings.Hera_Pay.CARD_TIME_TWO}
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  onPress={() => {
+                    Platform.OS === 'ios' ? backAction() : setShowModal(true);
+                  }}>
+                  <Image source={Images.iconDarkMore} />
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity style={styles.addCardContainer}>
+                <Text style={styles.addCardTxt}>
+                  {Strings.Hera_Pay.ADD_CARD}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </Container>
+      {modal && (
+        <CustomModal>
+          <AboutPayment onPress={() => setModal(!modal)} />
+        </CustomModal>
+      )}
+      <ModalMiddle
+        showModal={showModal}
+        onRequestClose={() => {
+          setShowModal(!showModal);
+        }}
+        String_1={Strings.Hera_Pay.Remove_Card}
+        String_2={Strings.Hera_Pay.Remove_Card_Text}
+        String_3={Strings.Hera_Pay.Yes_Remove}
+        String_4={Strings.Hera_Pay.Not_Now}
+        onPressNav={() => {
+          setShowModal(false);
+        }}
+        onPressOff={() => {
+          setShowModal(false);
+        }}
+      />
+    </>
+  );
+};
+
+export default React.memo(HeraPay);
---
+++ src/screens/auth/smdonor/heraPay/style.js
@@ -0,0 +1,97 @@
+import {Alignment, Colors} from '../../../../constants';
+import {Fonts} from '../../../../constants/Constants';
+import {Value} from '../../../../constants/FixedValues';
+import {
+  dynamicSize,
+  px,
+  scaleHeight,
+  scaleWidth,
+  statusHide,
+} from '../../../../utils/responsive';
+
+export default {
+  flex: {flex: Value.CONSTANT_VALUE_1, backgroundColor: Colors.BACKGROUND},
+  headerIcon: {
+    paddingHorizontal: Value.CONSTANT_VALUE_5,
+  },
+  container: {
+    flex: 1,
+    marginTop: statusHide(Value.CONSTANT_VALUE_105),
+  },
+  heraPay: {
+    color: Colors.BLACK,
+    fontFamily: Fonts.OpenSansBold,
+    letterSpacing: 2.84,
+    textAlign: Alignment.CENTER,
+    fontSize: Value.CONSTANT_VALUE_11,
+  },
+  sendPay: {
+    fontFamily: Fonts.OpenSansBold,
+    fontSize: Value.CONSTANT_VALUE_23,
+    textAlign: Alignment.CENTER,
+    color: Colors.BLACK,
+    marginTop: dynamicSize(Value.CONSTANT_VALUE_8),
+  },
+  btnContainer: {
+    width: px(Value.CONSTANT_VALUE_300),
+    backgroundColor: Colors.GREEN,
+    borderRadius: scaleWidth(Value.CONSTANT_VALUE_40),
+    height: scaleHeight(Value.CONSTANT_VALUE_80),
+    justifyContent: Alignment.CENTER,
+    alignSelf: Alignment.CENTER,
+    marginBottom: scaleHeight(Value.CONSTANT_VALUE_25),
+    marginTop: scaleHeight(Value.CONSTANT_VALUE_54),
+  },
+  btnText: {
+    textAlign: Alignment.CENTER,
+    color: Colors.COLOR_535858,
+    fontSize: Value.CONSTANT_VALUE_14,
+    fontFamily: Fonts.OpenSansBold,
+    letterSpacing: 1.8,
+  },
+  paymentReqContainer: {
+    marginTop: dynamicSize(Value.CONSTANT_VALUE_20),
+  },
+  historyContainer: {
+    marginTop: dynamicSize(Value.CONSTANT_VALUE_25),
+  },
+  cerditCardContainer: {
+    marginTop: dynamicSize(Value.CONSTANT_VALUE_30),
+  },
+  cardsContainer: {
+    flexDirection: Alignment.ROW,
+    alignItems: Alignment.CENTER,
+    justifyContent: Alignment.SPACE_BETWEEN,
+  },
+  cardsInner: {
+    marginRight: dynamicSize(Value.CONSTANT_VALUE_89),
+  },
+  cardNo: {
+    fontFamily: Fonts.OpenSansBold,
+    fontSize: Value.CONSTANT_VALUE_16,
+    lineHeight: Value.CONSTANT_VALUE_21,
+    color: Colors.BLACK,
+  },
+  cardTime: {
+    fontFamily: Fonts.OpenSansRegular,
+    fontSize: Value.CONSTANT_VALUE_13,
+    lineHeight: Value.CONSTANT_VALUE_21,
+    color: Colors.BLACK,
+  },
+  cardsTwoContainer: {
+    flexDirection: Alignment.ROW,
+    alignItems: Alignment.CENTER,
+    justifyContent: Alignment.SPACE_BETWEEN,
+    marginTop: dynamicSize(Value.CONSTANT_VALUE_40),
+  },
+  addCardContainer: {
+    marginTop: dynamicSize(Value.CONSTANT_VALUE_25),
+    marginBottom: dynamicSize(Value.CONSTANT_VALUE_95)
+  },
+  addCardTxt:{
+    fontFamily: Fonts.OpenSansBold,
+    fontSize: Value.CONSTANT_VALUE_14,
+    lineHeight: Value.CONSTANT_VALUE_21,
+    color: Colors.BLACK,
+  },
+};
