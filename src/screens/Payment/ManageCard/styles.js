24eb965d07f6d70613bebba6454b9ae391340052
Prerna-Kiwi <Prerna.sharma@kiwitech.com>
2023-05-03 15:33:04 +0530

    'Add Card'

--- src/screens/Payment/ManageCard/styles.js
+++ src/screens/Payment/ManageCard/styles.js
@@ -1,5 +1,5 @@
 import {Value} from '../../../constants/FixedValues';
-import {statusHide} from '../../../utils/responsive';
+import {dynamicSize, statusHide} from '../../../utils/responsive';
 import {Alignment, Colors} from '../../../constants';
 import {Fonts} from '../../../constants/Constants';
 
@@ -14,6 +14,7 @@ export default {
   mainContainer: {
     flex: Value.CONSTANT_VALUE_1,
     marginTop: statusHide(104),
+    marginHorizontal: dynamicSize(Value.CONSTANT_VALUE_40),
   },
   mainText: {
     color: Colors.BLACK,
@@ -21,5 +22,17 @@ export default {
     letterSpacing: 2.84,
     textAlign: Alignment.CENTER,
     fontSize: Value.CONSTANT_VALUE_11,
+  },
+  cardDetails: {
+    fontFamily: Fonts.OpenSansBold,
+    fontSize: Value.CONSTANT_VALUE_23,
+    textAlign: Alignment.CENTER,
+    color: Colors.BLACK,
+    marginTop: dynamicSize(Value.CONSTANT_VALUE_8),
+  },
+  addBtn: {
+    height: 80,
+    marginTop: Value.CONSTANT_VALUE_50,
+    marginBottom: Value.CONSTANT_VALUE_20,
   },
 };
